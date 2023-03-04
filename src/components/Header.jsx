81b0088974ca43b7e5738e390c0a5a2774a1dfcd
Tiaanz <jwtian126@gmail.com>
2023-03-04 18:40:53 +1300

    Hangman-React v1.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+
+ const Header = () => {
+  return (
+    <div>
+      <Typography variant="h2" align="center" sx={{ margin: '100px 0' }}>
+        HangMan
+      </Typography>
+    </div>
+  )
+ }
+
+ export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,143 @@
+import React from 'react'
+import { useState } from 'react'
+import { Typography, Input, Paper, Button, Box } from '@mui/material'
+import data from '../data/wordsData.json'
+
+function getRamdonIndex(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length)
+  return randomIndex
+}
+
+const hints = Object.keys(data)
+const hintIndex = getRamdonIndex(hints)
+
+const wordsArray = data[hints[hintIndex]]
+const ramdonWordsIndex = getRamdonIndex(wordsArray)
+const lettersArray = wordsArray[ramdonWordsIndex].split('')
+const initialArray = lettersArray.map((item) => '___')
+
+const Main = () => {
+  const [dynamicArray, setDynamicArray] = useState(initialArray)
+  const [count, setCount] = useState(8)
+  const [guess, setGuess] = useState('')
+  const [showWin, setShowWin] = useState(false)
+  const [showLose, setShowLose] = useState(false)
+  let guessArr = [...dynamicArray]
+
+  const checkLetter = (letter) => (e) => {
+    e.preventDefault()
+    lettersArray.forEach((item, i) => {
+      if (item === letter) {
+        guessArr[i] = letter
+        setGuess('')
+      }
+    })
+
+    if (!guessArr.includes(letter)) {
+      if (guess) {
+        if (count > 1) {
+          setCount(count - 1)
+          setGuess('')
+        } else {
+          setCount(count - 1)
+          setShowLose(true)
+        }
+      }
+    }
+    if (checkWin(guessArr)) {
+      setShowWin(true)
+    }
+    setDynamicArray(guessArr)
+  }
+
+  const checkWin = (arr) => {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== lettersArray[i]) return false
+      }
+      return true
+  }
+
+  const reset = () => {
+    setCount(8)
+    setShowLose(false)
+    setShowWin(false)
+    setGuess('')
+    setDynamicArray(initialArray)
+  }
+
+  return (
+    <>
+      <Box sx={{ textAlign: 'center' }}>
+        <Typography variant="h5" sx={{ color: 'green', margin: '30px 0' }}>
+          Hint:{hints[hintIndex]}
+        </Typography>
+
+        {dynamicArray.map((item, index) => (
+          <Typography
+            variant="h5"
+            key={index}
+            sx={{ display: 'inline', color: 'blue' }}
+          >
+            {item.toLocaleUpperCase()}&nbsp;&nbsp;&nbsp;
+          </Typography>
+        ))}
+      </Box>
+      {count >= 0 ? (
+        <Typography variant="h6" align="center" sx={{ margin: '30px 0' }}>
+          Guesses left: {count}
+        </Typography>
+      ) : (
+        <Typography variant="h6" align="center" sx={{ margin: '30px 0' }}>
+          Guesses left: 0
+        </Typography>
+      )}
+
+      <Paper elevation={0} sx={{ justifyContent: 'center', display: 'flex' }}>
+        <form onSubmit={checkLetter(guess)}>
+          <Input
+            id="guess"
+            placeholder="Enter your guess"
+            name="guess"
+            autoFocus
+            value={guess}
+            onChange={(e) => setGuess(e.target.value)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            size="small"
+            sx={{ margin: '20px 10px' }}
+          >
+            Submit
+          </Button>
+          <Box sx={{ textAlign: 'center', margin: '10px 0' }}>
+            {((showLose && count === 0) || showWin) && (
+              <Button
+                color="secondary"
+                variant="contained"
+                onClick={reset}
+                size="small"
+              >
+                Restart
+              </Button>
+            )}
+          </Box>
+        </form>
+      </Paper>
+      <Paper elevation={0} sx={{ margin: '20px' }}>
+        {showWin && (
+          <Typography align="center" variant="h6" sx={{ color: 'green' }}>
+            You Won!!!
+          </Typography>
+        )}
+        {showLose && (
+          <Typography align="center" variant="h6" sx={{ color: 'orange' }}>
+            Try again please
+          </Typography>
+        )}
+      </Paper>
+    </>
+  )
+}
+
+export default Main
